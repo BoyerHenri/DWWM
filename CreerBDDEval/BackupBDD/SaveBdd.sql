2491ce9eba3cb2e0d4e252520881a179d70423c7
BoyerHenri <boyerhenri@sfr.fr>
2021-08-17 10:23:13 +0200

    Save/res

---
+++ CreerBDDEval/BackupBDD/SaveBdd.sql
@@ -0,0 +1,14 @@
+-- N'ayant pas eu la correction de ma base de données, j'ai effectué la
+-- sauvegarde/restauration avec la base test "BddGescom_Afpa_Complete"
+
+-- *********
+-- Backup :
+-- Dans le dossier de reception du backup voulu, dans un terminal :
+
+mysqldump --user=root --password=root gescom_afpa > backuptest.sql;
+
+-- Il y a deux backups, ayant fait un essai je ne voulais pas corrompre une sauvegarde
+
+-- ************
+-- Resaturation
+mysql --user=root --password=root < backuptest.sql;
