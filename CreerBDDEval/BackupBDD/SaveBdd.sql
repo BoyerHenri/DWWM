6c33e37d45717b99972878ce7658cf594d516641
BoyerHenri <boyerhenri@sfr.fr>
2021-08-17 11:10:28 +0200

    MAJ

--- CreerBDDEval/BackupBDD/SaveBdd.sql
+++ CreerBDDEval/BackupBDD/SaveBdd.sql
@@ -5,10 +5,10 @@
 -- Backup :
 -- Dans le dossier de reception du backup voulu, dans un terminal :
 
-mysqldump --user=root --password=root gescom_afpa > backuptest.sql;
+mysqldump --user=root --password=root gescom_afpa --databases > Backup_afpa.sql;
 
 -- Il y a deux backups, ayant fait un essai je ne voulais pas corrompre une sauvegarde
 
 -- ************
 -- Resaturation
-mysql --user=root --password=root < backuptest.sql;
+mysql --user=root --password=root < Backup_afpa.sql;
