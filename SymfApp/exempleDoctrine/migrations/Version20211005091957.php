f0c13bb294c69546026c2d818032e18e9d96cf86
BoyerHenri <boyerhenri@sfr.fr>
2021-10-05 16:22:58 +0200

    Symfo Doctrine

---
+++ SymfApp/exempleDoctrine/migrations/Version20211005091957.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211005091957 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE orderdetails (id INT AUTO_INCREMENT NOT NULL, unit_price NUMERIC(10, 4) NOT NULL, quantity SMALLINT NOT NULL, discount DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE customers ADD yes LONGTEXT NOT NULL, CHANGE phone phone LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE orderdetails');
+        $this->addSql('ALTER TABLE customers DROP yes, CHANGE phone phone VARCHAR(24) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
